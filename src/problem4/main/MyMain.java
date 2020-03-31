e5c824c2f6fc59b4eca753dad005cea2751fc054
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-03-31 17:44:50 +0530

    creating object obj of MyBinarySearchTree class

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,10 +5,13 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
     public static void main(String[] args) {
-
+        MyBinarySearchTree obj = new MyBinarySearchTree();
     }
 }
