ee4e16c012527853572ccb7bbf0775ceea2ca9d2
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-03-31 17:47:15 +0530

    creating the Node class for MyQueue class

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -8,4 +8,24 @@ package problem4.myqueue;
 // to create queue to store pre - order successor
 
 public class MyQueue {
+    class Node {
+        int data;
+        Node next;
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
