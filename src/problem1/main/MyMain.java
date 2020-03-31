01cfeb3facd968b08f34ab5408477d783af7706d
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-03-31 11:39:09 +0530

    calling MyBinarySearchTree class

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,8 +7,10 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
     public static void main(String[] args) {
-
+        MyBinarySearchTree obj = new MyBinarySearchTree();
     }
 }
