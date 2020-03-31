7e3dbeaf4d7f5cac18929e7c2ca2ecbb364ecdce
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-03-31 18:54:51 +0530

    creating display() method to print the information

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -49,4 +49,14 @@ public class MyCircularQueue {
             }
         }
     }
+
+    public void display() {
+        Node temp = front;
+        System.out.println();
+        while (temp.getNext() != front) {
+            System.out.println(temp.getStudent());
+            temp = temp.getNext();
+        }
+        System.out.println(temp.getStudent());
+    }
 }
