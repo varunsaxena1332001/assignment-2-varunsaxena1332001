1cf2d20ea5e0c06d82977a9c14600c44a6209ff8
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-03-31 12:02:01 +0530

    creating main method

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -8,4 +8,7 @@ package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+
+    }
 }
