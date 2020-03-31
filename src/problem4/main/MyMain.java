184823a0a83933daa193aec3e418467a2e6242d1
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-03-31 17:56:40 +0530

    creating object mq of MyQueue class

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,11 +7,14 @@
 package problem4.main;
 
 import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
 
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree obj = new MyBinarySearchTree();
+
+        MyQueue mq = new MyQueue();
     }
 }
