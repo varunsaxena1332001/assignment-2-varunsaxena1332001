9f34419083785611da2eb19107ccb776d6f279d8
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-03-31 12:12:55 +0530

    creating main method

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -8,4 +8,7 @@ package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+
+    }
 }
