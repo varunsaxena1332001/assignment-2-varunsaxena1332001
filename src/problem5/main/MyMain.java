efe7ce3c087696769c1957a2a1800a3459f71ceb
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-03-31 18:57:39 +0530

    performing some operations such as remove the backLog and display the value

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -12,5 +12,14 @@ import problem5.circularqueue.MyCircularQueue;
 public class MyMain {
     public static void main(String[] args) {
         MyCircularQueue obj = new MyCircularQueue();
+        obj.enqueue(1, 4, 0);
+        obj.enqueue(2, 4, 1);
+        obj.enqueue(3, 4, 4);
+        obj.enqueue(4, 2, 1);
+        obj.enqueue(5, 2, 2);
+        obj.enqueue(6, 0, 0);
+        obj.removeBackLog();
+        obj.display();
+
     }
 }
