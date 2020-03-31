16ca351a3e75ba4459882a21d402cef53d4c3098
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-03-31 18:07:06 +0530

    creating constructor of MyCircularQueue class of public type

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,18 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.node.Node;
+
 //to implement circular queue
 public class MyCircularQueue {
+    Node front;
+    Node rear;
+    int count;
+
+    public MyCircularQueue() {
+        front = null;
+        rear = null;
+        count = 0;
+    }
 }
