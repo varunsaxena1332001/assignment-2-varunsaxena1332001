a04ab0be041b7ed639c91f25cecb28964f7f7b01
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-03-31 12:01:07 +0530

    performing some operations on MyBinarySearchTree class

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -13,5 +13,13 @@ import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(5);
+        obj.insert(10);
+        obj.insert(1);
+        obj.insert(6);
+        obj.insert(50);
+        obj.insert(40);
+        obj.insert(30);
+        obj.preVSpost();
     }
 }
