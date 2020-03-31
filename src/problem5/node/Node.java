669f05ebcbe98309b0aa80ba32a929a9109d911e
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-03-31 18:05:39 +0530

    creating Node class for Student type data

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -6,6 +6,26 @@
  */
 package problem5.node;
 
+import problem5.student.Student;
+
 // to define node properties
 public class Node {
+    Student student;
+    Node next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
